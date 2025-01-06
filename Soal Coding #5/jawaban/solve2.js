13b14ce9a141c4f8fa574343be27a796379d6353
faishalkamali <111752556+iskhafel@users.noreply.github.com>
2025-01-06 13:32:18 +0700

    update

--- Soal Coding #5/jawaban/solve2.js
+++ Soal Coding #5/jawaban/solve2.js
@@ -1,32 +1,18 @@
-/**
- * selection sort (asc / desc)
- */
+function sumDeep(char, arr, level = 1) {
+  let sum = 0;
 
-function selectionSortAsc(arr) {
-  // tulis code di sini
-  while (true) {
-    let tukeran = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i + 1] > arr[i]) {
-        let temp = arr[i + 1];
-        arr[i + 1] = arr[i];
-        arr[i] = temp;
-        tukeran = true;
+  for (const node of arr) {
+    if (typeof node === "string") {
+      for (let j = 0; j < node.length; j++) {
+        if (node[j] === char) {
+          sum += level;
+          break;
+        }
       }
-    }
-    if (tukeran === false) {
-      break;
+    } else if (Array.isArray(node)) {
+      sum += sumDeep(char, node, level + 1);
     }
   }
-  return arr;
+
+  return sum;
 }
-
-function selectionSortDesc(arr) {
-  // tulis code di sini
-}
-
-let angka = [1, 5, 23, 3, 5, 6, 3, 3, 45, 6, 53, 2, 6, 34, 523, 912];
-
-console.log("dari kecil ke besar :", selectionSortAsc(angka));
-
-console.log("dari besar ke kecil :", selectionSortDesc(angka));
